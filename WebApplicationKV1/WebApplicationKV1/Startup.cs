17499d2974a8270978b79339194dfcf35fac22b0
Chrisk130 <chriskeenan22@gmail.com>
2016-01-06 15:27:15 -0500

    First Upload

---
+++ WebApplicationKV1/WebApplicationKV1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationKV1.Startup))]
+namespace WebApplicationKV1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
